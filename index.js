6d99a622afdcdd5e8e25f4d26bdc1eb8e8fbe316
anetastankovska <anetastankovska@users.noreply.github.com>
2026-07-02 20:23:30 +0000

    deploy: 8c3834c64a29eacf19628a06d15fb687db065e94

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
 const IS_LOCAL = window.location.hostname === "localhost" || window.location.hostname === "127.0.0.1";
 const API_URL = IS_LOCAL 
   ? "http://localhost:3030/api/students" 
- : "https://api.render.com/deploy/srv-d871c9a8qa3s73cejik0?key=U963NwJwjos";
+ : "https://my-app-backend-ihw2.onrender.com/api/students";
 
 const fetchStudents = async (url) => {
   const response = await fetch(url);
